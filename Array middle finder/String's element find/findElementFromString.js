6d8416f424f94097659a2e4a14e9b0dbf98696a0
tanvir35web <islammdtanvirul@gmail.com>
2024-01-01 09:35:00 +0600

    find number of "sd" repeated on string

--- Array middle finder/String's element find/findElementFromString.js
+++ Array middle finder/String's element find/findElementFromString.js
@@ -1,11 +1,17 @@
-const str = "nothing";
+//find number of "sd" repeated on string
+
+const str = "asdsaldasd";
 
 let counter = 0;
 
 for (i = 0; i < str.length; i++) {
   if (str[i] === "s") {
-    counter++;
+    if(str[i+1] === "d"){
+        counter ++;
+    }
   }
 }
 console.log(str);
 console.log(counter);
+
+
