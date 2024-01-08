7736855bd0c3c7b383e862edbfa44e0f40cd9b25
tanvir35web <islammdtanvirul@gmail.com>
2024-01-09 01:45:45 +0600

    leetcode 1512 solved

---
+++ Leetcode/1512.js
@@ -0,0 +1,13 @@
+var numIdenticalPairs = function(nums) {
+    let count = 0;
+    for(let i = 0; i< nums.length; i++){
+        for(let j = i+1; j< nums.length; j++){
+            if(nums[i] === nums[j]){
+                count += 1
+            }
+        }
+    }
+    return count 
+};
+
+console.log(numIdenticalPairs([1,2,3,1,1,3]));
