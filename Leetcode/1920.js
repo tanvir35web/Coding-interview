126b6dc514d791bb278d8363fee52a74bdfad78d
tanvir35web <islammdtanvirul@gmail.com>
2024-01-10 00:13:31 +0600

    1920. Build Array from Permutation

---
+++ Leetcode/1920.js
@@ -0,0 +1,12 @@
+var buildArray = function(nums) {
+    ans = [];
+    for(i = 0; i < nums.length; i++) {
+        answer = nums[nums[i]]
+        ans.push(answer)
+        
+    }
+    return ans;
+};
+
+nums = [5,0,1,2,3,4]
+console.log(buildArray(nums));
