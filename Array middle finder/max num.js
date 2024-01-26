22dc96aa3304079388c5003187dde5c2c0dd00cb
tanvir35web <islammdtanvirul@gmail.com>
2024-01-27 00:46:01 +0600

    max and min value from an array

---
+++ Array middle finder/max num.js
@@ -0,0 +1,14 @@
+
+function minValue(arr) {
+    let min = arr[0];
+
+    for(i=0; i < arr.length; i++) {
+        if(arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
+var arr = [4, 12, 3, 8, 0, -25,, 106, 56];
+console.log(minValue(arr));
