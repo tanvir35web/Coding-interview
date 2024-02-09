56b99743b126c87af6931b17dac04b51099bb5f8
tanvir35web <islammdtanvirul@gmail.com>
2024-02-10 00:01:59 +0600

    code optimized

--- Leetcode/2367.js
+++ Leetcode/2367.js
@@ -1,8 +1,9 @@
 var arithmeticTriplets = function(nums, diff) {
     counter = 0;
-    for(i=0; i < nums.length; i++) {
-        for(j = i + 1; j < nums.length; j++) {
-            for(k = j + 1; k < nums.length; k++) {
+    length = nums.length;
+    for(i=0; i < length; i++) {
+        for(j = i + 1; j < length; j++) {
+            for(k = j + 1; k < length; k++) {
                 if(nums[j] - nums[i] === diff && nums[k] - nums[j] === diff ) {
                     counter++;
                 }
