08ce8fe325f9cec431e368d5906b15e4f1c9f0fd
tanvir35web <islammdtanvirul@gmail.com>
2024-02-21 10:32:16 +0600

    leetcode 2006 solved

--- Leetcode/2006.js
+++ Leetcode/2006.js
@@ -1,16 +1,20 @@
-var countKDifference = function(nums, k) {
-    count = 0
-    for(i=0; i < nums.length; i++) {
-        for(j=i + 1; i < nums.length; j++){
-            if(nums[i] - nums[j] === k) {
-                count++;
+var countKDifference = function (nums, k) {
+    count = 0;
+    for (i = 0; i < nums.length; i++) {
+        for (j = i + 1; j < nums.length; j++) {
+            if (nums[i] > nums[j]) {
+                if (nums[i] - nums[j] === k) {
+                    count++;
+                }
             }
-
+            if (nums[i] < nums[j]) {
+                if (nums[j] - nums[i] === k) {
+                    count++;
+                }
+            }
         }
     }
-
     return count;
 };
 
-
-console.log(countKDifference([1,2,2,1], 1));
+console.log(countKDifference([3,2,1,5,4], 2));
