406a9ed930a8d7599fa3b86cd73c6b0650a53c5b
tanvir35web <islammdtanvirul@gmail.com>
2024-04-08 11:04:36 +0600

    leetcode 2703 solved using javascript

---
+++ Leetcode/2703.js
@@ -0,0 +1,11 @@
+var argumentsLength = function(...args) {
+
+    count = 0;
+    for (let i = 0; i < args.length; i++) {
+       count ++;
+    }
+    return count;
+};
+
+
+console.log(argumentsLength({}, null, "3", 50));
