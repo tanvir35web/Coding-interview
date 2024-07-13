af4922890e4b28aa196e54a2f8e7604c0334bc13
tanvir35web <islammdtanvirul@gmail.com>
2024-07-13 20:04:54 +0600

    word occarance related problem solved by self

---
+++ Array middle finder/word occarance.js
@@ -0,0 +1,29 @@
+function wordOccarance(str) {
+
+  // create a Hash map to store and see the output like object
+
+  let hashMap = {};
+
+  // capitalized or smallized all the character 
+  const capitalizedForm = str.toUpperCase();
+
+
+  // iterate the full sentence using loop
+  for(let i = 0; i < capitalizedForm.length; i++) {
+    let char = capitalizedForm[i];
+
+    //store every word standalone and checking every char, if already exist the count + 1  the current word otherwise set the value 1
+
+    if(char in hashMap){
+      hashMap[char] = hashMap[char] + 1;
+    } else {
+      hashMap[char] = 1;
+    }
+  }
+
+  //return the object
+  return hashMap;
+
+}
+
+console.log(wordOccarance("Developper"))
